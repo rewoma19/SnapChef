78920377cc2001f8a5c88c3f8c62515d936ed15e
Raphael <rewoma19@gmail.com>
2026-02-20 05:14:48 -0700

    Initial Commit

---
+++ SnapChef/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SnapChef
+//
+//  Created by Raphael Odeareduo on 2026-02-20.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SnapChef/SnapChefApp.swift
@@ -0,0 +1,32 @@
+//
+//  SnapChefApp.swift
+//  SnapChef
+//
+//  Created by Raphael Odeareduo on 2026-02-20.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SnapChefApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
